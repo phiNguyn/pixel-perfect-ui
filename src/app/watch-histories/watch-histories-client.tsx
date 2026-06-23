f1f5a9ac80627315897f09d7400b85ddcc254c23
phiNguyn <162539991+phiNguyn@users.noreply.github.com>
2026-06-23 19:13:27 +0700

    Change grid layout from 2 to 3 columns

--- src/app/watch-histories/watch-histories-client.tsx
+++ src/app/watch-histories/watch-histories-client.tsx
@@ -167,7 +167,7 @@ export default function WatchHistoriesClient() {
 
       {/* Grid View */}
       {viewMode === "grid" ? (
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+        <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
           <AnimatePresence>
             {sortedHistory.map((item) => (
               <WatchHistoryGridCard
